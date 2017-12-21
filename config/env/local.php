1cf23fc0c7f8bc443fb2ea9b7aee7d1b375e5392
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-12-21 11:53:48 +0000

    Set S3_UPLOADS_USE_LOCAL to false on local.php

--- config/env/local.php
+++ config/env/local.php
@@ -2,9 +2,9 @@
 /**
  * Local environment config
  */
-define( 'SAVEQUERIES', true );
-define( 'WP_DEBUG', true );
-define( 'SCRIPT_DEBUG', true );
+define('SAVEQUERIES', true);
+define('WP_DEBUG', true);
+define('SCRIPT_DEBUG', true);
 
 // disable s3 uploads locally
-define( 'S3_UPLOADS_USE_LOCAL', true );
+define('S3_UPLOADS_USE_LOCAL', false);
