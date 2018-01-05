2ff99b522d4c72c54dfddcafc93711605ddcc868
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-01-05 15:26:53 +0000

    Remove .git folder from the copy

--- tools/new_project.sh
+++ tools/new_project.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/Clarkom/wp-theme-deploy.git
 mv wp-theme-deploy $1
 cd $1
+rm -r .git
 cp .env.sample .env
 mv htdocs/wp-content/themes/projectname-theme htdocs/wp-content/themes/$1-theme
 for file in $(grep -R 'projectname' -l --exclude-dir=htdocs/wp-content/themes/$1-theme); do $(sed -i s/projectname/$1/g "$file"); done
