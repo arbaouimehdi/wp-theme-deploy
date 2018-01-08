ce8696a823a9939f861c0c8eafa43fcd04383632
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-01-08 12:13:03 +0000

    Add table prefix option

--- tools/new_project.sh
+++ tools/new_project.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# $1 project name
+# $2 wordpress table prefix
+
 git clone https://github.com/Clarkom/wp-theme-deploy.git
 mv wp-theme-deploy $1
 cd $1
@@ -7,4 +10,5 @@ rm -r .git
 cp .env.sample .env
 mv htdocs/wp-content/themes/projectname-theme htdocs/wp-content/themes/$1-theme
 for file in $(grep -R 'projectname' -l --exclude-dir=htdocs/wp-content/themes/$1-theme); do $(sed -i s/projectname/$1/g "$file"); done
+sed -i 's/\<wp_\>/'$2'/g' config/wp-config.php
 sudo npm run shell
